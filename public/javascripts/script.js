286cda387b9e720b0f7a1565c01645f103fa01fd
tupyck <rackovilijaa@gmail.com>
2020-05-03 22:47:31 +0200

    test

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,8 +3,6 @@ const messageCardTemplate = document.querySelector("#mc-index").content;
 /// Container where all of users messages are stored
 const userMessages = document.querySelector("#cards-container");
 
-console.log("ja sam cigan");
-
 /// Adding a new message to the scrollable container
 function newMessage() {
     const newCard = document.importNode(messageCardTemplate, true);
